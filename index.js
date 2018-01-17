17f33efe019fcf0df3b78da2fea2aea5874a4636
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2018-01-17 20:01:38 +0100

    Hotfix

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let accessAllowed;
+let age = prompt('How old are you?', '');
+
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+
+alert(accessAllowed);
